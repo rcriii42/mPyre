da926a85938f9b1075cdb4bb4388b6916b5c0d59
rcriii42 <rcriii@ramsdells.net>
2019-12-22 17:02:35 -0600

    Coment out or delete unused stuff

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # this is a placeholder file
-# Viewer
 import Controller
 from GameWindow import GameWindow
-import pygame
-from pygame.locals import NOEVENT, QUIT
+
 
 if __name__ == "__main__":
     GW = GameWindow()
