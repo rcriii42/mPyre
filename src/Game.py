3c36628b5ff7dcbdc3bdcd01869c7923676a09ee
rcriii42 <rcriii@ramsdells.net>
2019-12-22 16:59:43 -0600

    Coment out or delete unused stuff

--- src/Game.py
+++ src/Game.py
@@ -7,7 +7,6 @@ from City import City
 from Player import Player
 
 Demo = True
-days_to_hours = 24.
 
 
 def comprehension_flatten(iter_lst):
@@ -18,24 +17,19 @@ class Game(object):
     """Holds the various game objects"""
 
     def __init__(self):
-        self.date = datetime.datetime(1920, 1, 1)
+        self.turn = 1
 
-        possible_cities = [City(self.date, "Boston", (530, 10)),
-                           City(self.date, "New York", (390, 120)),
-                           City(self.date, "Norfolk", (280, 360)),
-                           City(self.date, "Jacksonville", (15, 750)),
-                           ]
-        self.cities = random.sample(possible_cities, 3)
-
-        self.players = [Player()]
-        self.date = datetime.datetime(1920, 1, 1)
+        # possible_cities = [City(self.date, "Boston", (530, 10)),
+        #                    City(self.date, "New York", (390, 120)),
+        #                    City(self.date, "Norfolk", (280, 360)),
+        #                    City(self.date, "Jacksonville", (15, 750)),
+        #                    ]
+        # self.cities = random.sample(possible_cities, 3)
+        #
+        # self.players = [Player()]
 
     @property
-    def dredges(self):
+    def units(self):
         """return a list of all dredges"""
-        return comprehension_flatten([p.dredges for p in self.players])  # this returns a flat list of all dredges
+        return comprehension_flatten([p.units for p in self.players])  # this returns a flat list of all units
 
-    @property
-    def projects(self):
-        """return a list of projects"""
-        return comprehension_flatten([c.projects for c in self.cities])  # this returns a flat list of projects underway
