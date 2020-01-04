e5002eb8afade21378721c87c45239902b2494eb
rcriii42 <rcriii@ramsdells.net>
2020-01-04 16:59:36 -0600

    The City object

---
+++ src/Cities.py
@@ -0,0 +1,24 @@
+"""
+Cities - Contain s the code for City objects
+"""
+import os
+import pygame
+from BaseObjects import Space
+
+city_tile_path = os.path.join('graphics', 'city_tile_32x32.png')
+
+
+class City(Space):
+    """
+    Builds units and must be conquered to win the game.
+    """
+
+    def __init__(self, name="anytown", coords=(0, 0)):
+        self.coords = coords
+        self.name = name
+
+        image = pygame.image.load(os.path.join('graphics', 'city_tile_32x32.png')).convert()
+        self.image_size = (32, 32)
+        self.image = pygame.transform.scale(image, self.image_size)
+        self.plane = None
+
