194e106aeec61e5a107899d797a9002b4caff290
rcriii42 <rcriii@ramsdells.net>
2020-01-05 15:17:33 -0600

    Support units

--- src/Player.py
+++ src/Player.py
@@ -12,8 +12,15 @@ class Player(object):
 
     def assign_city(self, city):
         """assign a city to the player"""
+        self.cities.append(city)
         city.owner = self
         city.set_image(self.color)
+
+    def assign_unit(self, unit):
+        """assign a unit to the player"""
+        self.units.append(unit)
+        unit.owner = self
+        unit.set_image(self.color)
 
     def turn_step(self, G):
         """Adjust the player to reflect moving forward in time.
