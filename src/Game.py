aee932f481f7081aa41e64b241248f421640d265
rcriii42 <rcriii@ramsdells.net>
2020-01-06 06:26:35 -0600

    Test a second player

--- src/Game.py
+++ src/Game.py
@@ -20,16 +20,18 @@ class Game(object):
     def __init__(self):
         self.turn = 1
 
-        possible_cities = [City("Boston", (530, 10)),
-                           City("New York", (390, 120)),
-                           City("Norfolk", (280, 360)),
-                           City("Jacksonville", (15, 750)),
+        possible_cities = [City("Boston", (17*32, 32)),
+                           City("New York", (13*32, 4*32)),
+                           City("Norfolk", (9*32, 11*32)),
+                           City("Jacksonville", (32, 25*32)),
                            ]
         self.cities = random.sample(possible_cities, 3)
 
-        self.players = [Player()]
+        self.players = [Player(), Player(name="Blue", color='blue')]
         self.players[0].assign_city(self.cities[0])
         self.players[0].assign_unit(Infantry(coords=(320,320)))
+        self.players[1].assign_city(self.cities[2])
+        self.players[1].assign_unit(Infantry(name="42nd grenadiers", coords=(18*32, 3*32)))
 
     @property
     def units(self):
