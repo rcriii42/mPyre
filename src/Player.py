ae2955cb6491f8ead3900a1ec429723aa842912c
rcriii42 <rcriii@ramsdells.net>
2020-01-17 19:53:04 -0600

    Return the next unit able to move

--- src/Player.py
+++ src/Player.py
@@ -33,3 +33,19 @@ class Player(object):
             ts_messages.extend(u.turn_step(G))
 
         return ts_messages
+
+    def next_to_move(self, u=None):
+        if u in self.units:
+            i = self.units.index(u)
+            after = [a for a in self.units[i:]]
+            for a in after:
+                if a is not u and a.moved < a.move_speed:
+                    return a
+            before = [b for b in self.units[:i]]
+            for b in before:
+                if b.moved < b.move_speed:
+                    return b
+        else:
+            return None
+
+
