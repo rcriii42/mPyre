f1ab3aa2db2f28018319f190a3e49a45f739becc
rcriii42 <rcriii@ramsdells.net>
2020-01-19 11:23:28 -0600

    Add license text to source files

--- src/BaseObjects.py
+++ src/BaseObjects.py
@@ -1,8 +1,28 @@
 """BaseObjects - Define some basic objects
 
-Space - the meta object for all objects that can be on a map
+Unit - the meta object for all objects that can be on a map
 Map - a dictionary holding all the spaces on a map
 """
+
+#     This part of Pyre, a python implementation of the game Empire
+#     Copyright (C) 2019  Robert C. Ramsdell III <rcriii42@gmail.com>
+#
+#     Pyre is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Pyre is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Pyre.  If not, see <https://www.gnu.org/licenses/>.
+#
+#     Work started on 19 December, 2019
+
+
 import os
 import pygame
 from GraphicUtils import colors
--- src/Game.py
+++ src/Game.py
@@ -1,5 +1,24 @@
 """Game.py - Contains the Game object that holds all objects in the game - players, cities, 
-   dredges, and the game Map."""
+   units, and the game Map."""
+
+#     This part of Pyre, a python implementation of the game Empire
+#     Copyright (C) 2019  Robert C. Ramsdell III <rcriii42@gmail.com>
+#
+#     Pyre is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Pyre is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Pyre.  If not, see <https://www.gnu.org/licenses/>.
+#
+#     Work started on 19 December, 2019
+
 
 import random
 from Cities import City
