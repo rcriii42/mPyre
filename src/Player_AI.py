83a41b6a33865655a4f5e9425d552547c2f3191f
rcriii42 <rcriii@ramsdells.net>
2020-02-08 12:26:53 -0600

    AI handles instances when there is a unit blocking the move
    Sort the target scores correctly

--- src/Player_AI.py
+++ src/Player_AI.py
@@ -21,6 +21,7 @@
 import pygame.time
 from pygame.locals import K_KP1, K_KP2, K_KP3, K_KP4, K_KP6, K_KP7, K_KP8, K_KP9
 import random
+from BaseObjects import Unit
 from Cities import City
 from GroundUnits import Infantry
 
@@ -52,7 +53,7 @@ class AI():
         'End_Turn': end my turn
         'select', Unit: select the given unit
         """
-        pygame.time.wait(250)
+        pygame.time.wait(450)
         self.moving_unit = self.player.next_to_move()
         if self.moving_unit and not self.moving_unit_selected:
             self.moving_unit_selected = True
@@ -60,29 +61,53 @@ class AI():
             return ['select', self.moving_unit]
         elif self.moving_unit:
             t = self.select_target(self.moving_unit)
-            dir = {(0, 1):K_KP2,
-                   (1, 1):K_KP3,
-                   (1, 0):K_KP6,
-                   (1, -1):K_KP9,
-                   (0, -1):K_KP8,
-                   (-1, -1):K_KP7,
-                   (-1, 0):K_KP4,
-                   (-1, 1):K_KP1}[self.moving_unit.direction_to(t)]
+            dir, key = self.move_unit(t)
+
             print("{} target is {}, moving {}".format(self.moving_unit.name,
                                                       t.name,
                                                       dir))
-            return ['move', self.moving_unit, dir]
+            return ['move', self.moving_unit, key]
         else:
             self.moving_unit_selected = False
             return ["End Turn"]
 
-    def move_towards(self, target):
-        "Move the unit towards the given target"
-
+    def move_unit(self, target):
+        """Move the moving_unit towards the target if it can"""
+        dir = self.moving_unit.direction_to(target)
+        while True:
+            u = self.moving_unit.check_collision((self.moving_unit.coords[0]+dir[0]*self.moving_unit.image_size[0],
+                                                  self.moving_unit.coords[1]+dir[1]*self.moving_unit.image_size[1]),
+                                                 self.game)
+            if isinstance(u, City):
+                break
+            elif isinstance(u, Unit):
+                if self.moving_unit.owner is u.owner:
+                    dir = {( 0,  1): ( 1,  1),  #adjust movement clockwise
+                           ( 1,  1): ( 1,  0),
+                           ( 1,  0): ( 1, -1),
+                           ( 1, -1): ( 0, -1),
+                           ( 0, -1): (-1, -1),
+                           (-1, -1): (-1,  0),
+                           (-1,  0): (-1,  1),
+                           (-1,  1): ( 0,  1)}[dir]
+                else:
+                    break
+            else:
+                break
+        key = {( 0,  1): K_KP2,
+               ( 1,  1): K_KP3,
+               ( 1,  0): K_KP6,
+               ( 1, -1): K_KP9,
+               ( 0, -1): K_KP8,
+               (-1, -1): K_KP7,
+               (-1,  0): K_KP4,
+               (-1,  1): K_KP1}[dir]
+        return dir, key
 
     def select_target(self, unit):
         """Select a target randomly from the """
         targets = self.find_targets(unit)
+        print(unit.name, targets[:5])
         return _choices(targets[0][:5], weights=targets[1][:5])[0]
 
     def find_targets(self, unit):
@@ -91,7 +116,7 @@ class AI():
         units = [u for u in self.game.units if u.owner is not self.player]
         scores = [(c, self.score_target(unit, c)) for c in cities]
         scores.extend([(u, self.score_target(unit, u)) for u in units])
-        scores.sort(key=lambda tup: tup[1])
+        scores.sort(key=lambda tup: tup[1], reverse=True)
         return [t[0] for t in scores], [t[1] for t in scores]
 
     def score_target(self, unit, target):
