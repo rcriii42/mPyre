76446ee78a24cc5921518e72e7343a34bf37cb34
rcriii42 <rcriii@ramsdells.net>
2020-02-09 12:04:42 -0600

    MapBuilder initial commit

---
+++ src/MapBuilder.py
@@ -0,0 +1,19 @@
+"""MapBuilder.py - Map generator"""
+
+#     This is the map generator script for mPyre, a python implementation of the game Empire
+#     Copyright (C) 2019  Robert C. Ramsdell III <rcriii42@gmail.com>
+#
+#     mPyre is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     mPyre is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with mPyre.  If not, see <https://www.gnu.org/licenses/>.
+#
+#     Work started on 9 Frbruary, 2020
