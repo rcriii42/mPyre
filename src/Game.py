213e6296c5877aa26dd3caa7d9fe94a898b1ae00
rcriii42 <rcriii@ramsdells.net>
2020-02-09 13:54:13 -0600

    Use the map builder and randomly name the players

--- src/Game.py
+++ src/Game.py
@@ -25,13 +25,17 @@ from Cities import City
 from Player import Player
 from GroundUnits import Infantry
 from Player_AI import AI
-from  BaseObjects import Map
+from  BaseObjects import Map, Namer
+import MapBuilder
 
 Demo = True
 
 
 def comprehension_flatten(iter_lst):
     return list(item for iter_ in iter_lst for item in iter_)
+
+player_namer = Namer(name_list=["Joe", "Svetlana", "Estefan", "Wang Xiu Ying"],
+                     number_names=False)
 
 
 class Game(object):
@@ -40,32 +44,27 @@ class Game(object):
     def __init__(self, size):
         self.turn = 1
 
-        possible_cities = [City("Boston", (17*32, 32)),
-                           City("New York", (13*32, 4*32)),
-                           City("Norfolk", (9*32, 11*32)),
-                           City("Jacksonville", (32, 19*32)),
-                           ]
-        self.cities = possible_cities
+        self.map, self.cities = MapBuilder.map_builder(size)
 
         self.neutral = Player(name="Neutral", color = "white")
         for c in self.cities:
             self.neutral.assign_city(c, build_unit=False)
 
-        self.players = [Player(),
-                        Player(name="Blue", color='blue'),
-                        Player(name="MLBBR", color='orange')]
+        self.players = [Player(name=player_namer.name_unit()),
+                        Player(name=player_namer.name_unit(), color='blue'),
+                        Player(name=player_namer.name_unit(), color='orange')]
 
-        self.players[0].assign_city(possible_cities[0])
-        self.players[0].assign_unit(Infantry(coords=(possible_cities[0].coords[0],
-                                                     possible_cities[0].coords[1])))
+        self.players[0].assign_city(self.cities[0])
+        self.players[0].assign_unit(Infantry(coords=(self.cities[0].coords[0],
+                                                     self.cities[0].coords[1])))
 
-        self.players[1].assign_city(possible_cities[-1])
-        self.players[1].assign_unit(Infantry(coords=(possible_cities[-1].coords[0],
-                                                     possible_cities[-1].coords[1])))
+        self.players[1].assign_city(self.cities[2])
+        self.players[1].assign_unit(Infantry(coords=(self.cities[2].coords[0],
+                                                     self.cities[2].coords[1])))
         self.players[1].AI = AI(self.players[1],
                                 self)
 
-        c = random.sample(possible_cities[1:3], 1)[0]
+        c = self.cities[4]
         self.players[2].assign_city(c)
         self.players[2].assign_unit(Infantry(coords=(c.coords[0],
                                                      c.coords[1])))
@@ -76,10 +75,7 @@ class Game(object):
 
         self.lost_players = []
 
-        self.map = Map(dims = size)
-        for x in range(18*32, 24*32, 32):
-            for y in range(32, 4*32, 32):
-                self.map[x, y] = "water"
+
 
     @property
     def size(self):
