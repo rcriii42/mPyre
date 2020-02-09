2051921a353306fc3f9ed0a97b1455b418d8c5e1
rcriii42 <rcriii@ramsdells.net>
2020-02-09 13:53:24 -0600

    Build a map with water and cities
    Game uses the map builder

--- src/MapBuilder.py
+++ src/MapBuilder.py
@@ -17,3 +17,78 @@
 #     along with mPyre.  If not, see <https://www.gnu.org/licenses/>.
 #
 #     Work started on 9 Frbruary, 2020
+
+import random
+
+from BaseObjects import Namer, Map
+from Cities import City
+
+
+
+city_name_list = ["Bree", "Chicago", "Hobbiton", "Farmington", "Gotham City", "Beijing",
+                  "Danville", "Mayberry", "Salzburg", "Quahog", "Sacramento", "Bedrock",
+                  "Hanoi","Ember", "Vero Beach", "Ankh-Morpork","Berlin", "Innsmouth",
+                  "Saratoga", "Godric's Hollow", "Can Tho", "Ilium", "Pacheco",
+                  "Lankhmar", "Barcelona", "Macondo", "Lafayette", "R'lyeh",  "Delft",
+                  "Berkeley", "Thneedville", "Washoe Valley", "El Dorado", "Baltimore",
+                  "Camelot", "Verona", "Eden", "Bloomingdale", "Hyerborea", "Nankan",
+                  "Troy", "Hellerup", "Atlantis", "Copenhagen", "Niflheim", "Taipei",
+                  "Phjola", "Clarendon Hills", "San Miguel", "Sodom", "Downers Grove",
+                  "Boston", "New York", "Norfolk", "Jacksonville"]
+
+city_namer = Namer(name_list=city_name_list, number_names=False)
+
+def map_builder(size, numcities=10, numwater=3, squaresize=32):
+    """Generate a map in the given game"""
+    map = Map(dims=size)
+    map, cities = add_cities(map, numcities, squaresize)
+    for i in range(numwater):
+        map = add_water(map, squaresize)
+
+    return map, cities
+
+def add_water(map, squaresize, min_dia=3):
+    """add water bodies to the map
+
+    min_dia is the diameter of the smallest water body"""
+    while True:
+        base_coords = (random.randint(0, map.dims[0]/squaresize)*squaresize,
+                       random.randint(0, map.dims[1]/squaresize)*squaresize)
+        if map[base_coords] == "plains":
+            map[base_coords] = 'water'
+            break
+    water_to_check = [base_coords]
+    for x in range(0, min_dia*squaresize, squaresize):
+        for y in range(0, min_dia*squaresize, squaresize):
+            coords = (x + base_coords[0] - int(min_dia/2),
+                      y + base_coords[1] - int(min_dia/2))
+            print("{} {}".format(coords, map[coords]))
+            if map[coords] == "plains":
+                map[coords] = "water"
+                water_to_check.append(coords)
+    return map
+
+
+def add_cities(map, numcities, squaresize):
+    """Add cities to the map
+
+    return a list of cites"""
+    city_list = []
+    coords_list = []
+    for i in range(numcities):
+        name = city_namer.name_unit()
+        while True:
+            coords = (random.randint(0, map.dims[0]/squaresize)*squaresize,
+                      random.randint(0, map.dims[1]/squaresize)*squaresize)
+            if coords not in coords_list and map[coords]=="plains":
+                break
+        coords_list.append(coords)
+        city_list.append(City(name, coords))
+        map[coords] = "city"
+
+    return map, city_list
+
+
+
+
+
