b815daf37ab65ae8b8396857552773896db17a01
rcriii42 <rcriii@ramsdells.net>
2020-02-09 12:04:22 -0600

    Add license text

--- src/choices.py
+++ src/choices.py
@@ -1,5 +1,23 @@
 """choices - replacement for random.choices function for python<3.6"""
 
+#     This is a utility script for mPyre, a python implementation of the game Empire
+#     Copyright (C) 2019  Robert C. Ramsdell III <rcriii42@gmail.com>
+#
+#     mPyre is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     mPyre is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with mPyre.  If not, see <https://www.gnu.org/licenses/>.
+#
+#     Work started on 8 February, 2020
+
 import random
 from itertools import accumulate as _accumulate, repeat as _repeat
 from bisect import bisect as _bisect
