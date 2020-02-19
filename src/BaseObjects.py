4fce4de7267da23026ca6139b977fc57ad059765
rcriii <rcriii42@gmail.com>
2020-02-19 05:50:43 -0600

    Add A* algorithm for path finding
    As a comment so far

--- src/BaseObjects.py
+++ src/BaseObjects.py
@@ -189,5 +189,55 @@ class Map(dict):
                                     xy[1] + y))
         return ne_list
 
-
+# function reconstruct_path(cameFrom, current)
+#     total_path := {current}
+#     while current in cameFrom.Keys:
+#         current := cameFrom[current]
+#         total_path.prepend(current)
+#     return total_path
+#
+#
+# def A_Star(start, goal, map, impassible):
+#     """// A* finds a path from start to goal.
+#
+#     map is the map object with terrain.
+#     impassible is a list of impassible terrain
+#
+#     from the wiki page: https://en.wikipedia.org/wiki/A*_search_algorithm"""
+#
+#     # The set of discovered nodes that may need to be (re-)expanded.
+#     # Initially, only the start node is known.
+#     open_set = set([start])
+#
+#     # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
+#     cameFrom := an empty map
+#
+#     // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+#     gScore := map with default value of Infinity
+#     gScore[start] := 0
+#
+#     // For node n, fScore[n] := gScore[n] + h(n).
+#     fScore := map with default value of Infinity
+#     fScore[start] := h(start)
+#
+#     while openSet is not empty
+#         current := the node in openSet having the lowest fScore[] value
+#         if current = goal
+#             return reconstruct_path(cameFrom, current)
+#
+#         openSet.Remove(current)
+#         for each neighbor of current
+#             // d(current,neighbor) is the weight of the edge from current to neighbor
+#             // tentative_gScore is the distance from start to the neighbor through current
+#             tentative_gScore := gScore[current] + d(current, neighbor)
+#             if tentative_gScore < gScore[neighbor]
+#                 // This path to neighbor is better than any previous one. Record it!
+#                 cameFrom[neighbor] := current
+#                 gScore[neighbor] := tentative_gScore
+#                 fScore[neighbor] := gScore[neighbor] + h(neighbor)
+#                 if neighbor not in openSet
+#                     openSet.add(neighbor)
+#
+#     // Open set is empty but goal was never reached
+#     return failure
 
